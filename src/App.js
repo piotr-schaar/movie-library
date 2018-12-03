dc7764c55e51b039a2ede4e3904613243a360c05
piotr-schaar <piotr.schaar@gmail.com>
2018-12-03 02:10:00 +0100

    exact routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Movie from "./components/movie/Movie";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-            <Route path={'/'} component={Home} />
+          <Route exact path={"/"} component={Home} />
+          <Route path={"/movie/:id"} component={Movie} />
         </Switch>
       </BrowserRouter>
     );
--- src/components/movies/MoviesItem.js
+++ src/components/movies/MoviesItem.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class MoviesItem extends Component {
-    render() {
-        let index = this.props.index;
-        let movies = this.props.movies;
-        return (
-            <li key={index} className="movies__item">
-            <img
-              src={
-               movies[index].poster_path === null
-                  ? "http://via.placeholder.com/300x450"
-                  : `https://image.tmdb.org/t/p/w300/${movies[index].poster_path}`
-              }
-              alt={``}
-            />
-          </li>
-        );
-    }
+  render() {
+    let index = this.props.index;
+    let movies = this.props.movies;
+    return (
+      <Link
+        to={`/movie/${movies[index].id}`}
+        key={index} 
+      >
+        <li className="movies__item">
+          <img
+            src={
+              movies[index].poster_path === null
+                ? "http://via.placeholder.com/300x450"
+                : `https://image.tmdb.org/t/p/w300/${movies[index].poster_path}`
+            }
+            alt={``}
+          />
+          <div className="movies__info">
+            <h3>{movies[index].title}</h3>
+            <p>{movies[index].release_date}</p>
+          </div>
+        </li>
+      </Link>
+    );
+  }
 }
 
 export default MoviesItem;
