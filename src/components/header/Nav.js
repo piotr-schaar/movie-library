e5a4d9d51fa745353f545c93c129d6952ae59dc7
piotr-schaar <piotr.schaar@gmail.com>
2018-12-07 18:36:57 +0100

    create top-rated comp

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
     render() {
         return (
             <ul>
-                <li></li>
+                <li>
+                    <Link
+                        to={'/moviesRated'}
+                    >Top Rated</Link>
+
+                </li>
             </ul>
         );
     }
