eacc009775e86886c0420476c43df3151fc03dd3
piotr-schaar <piotr.schaar@gmail.com>
2018-12-07 22:15:27 +0100

    add comp moviesrated

--- src/components/movies/MoviesRated.js
+++ src/components/movies/MoviesRated.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import MoviesItem from "./MoviesItem";
 class MoviesRated extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: true,
+      movies: []
+    };
+  }
+  getData() {
+    const key = "bd5f28af222edabf18f21f9cf5683cca";
+    let url = `https://api.themoviedb.org/3/discover/movie/?api_key=${key}&language=en-US&certification_country=US&certification=R&sort_by=vote_average.desc`;
+  }
+  render() {
+    return (
+      <div className="movies newMovies">
+        <ul className="movies__list">
+          {this.state.movies.map((movie, index) => {
+            return (
+              <MoviesItem
+                key={index}
+                movie={movie}
+                index={index}
+                movies={this.state.movies}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default MoviesRated;
